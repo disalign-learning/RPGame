a882660fb88afc198608bc5ae2793796d656703b
disalign-learning <disalignforever@gmail.com>
2023-12-07 01:26:07 -0500

    first commit

---
+++ src/playerCharacter.java
@@ -0,0 +1,21 @@
+
+public class playerCharacter 
+{
+	int hp;
+	int max_hp;
+	
+	int atk;
+	int base_atk;
+	
+	int def;
+	int base_def;
+	
+	int coins;
+	int max_coins;
+	
+	
+	public int playerCharacter(int max_hp, int base_atk, int base_def, int max_coins) 
+	{
+		
+	}
+}
---
+++ src/rpgMain.java
@@ -0,0 +1,9 @@
+
+public class rpgMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		playerCharacter player = new playerCharacter();
+	}
+
+}
