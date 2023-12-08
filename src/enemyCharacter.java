68a52065252a181b99208738b7f79fe7a12825f8
disalign-learning <disalignforever@gmail.com>
2023-12-07 22:27:07 -0500

    previous git upload didn't work as intended. Reuploading.

--- src/enemyCharacter.java
+++ src/enemyCharacter.java
@@ -1,8 +1,31 @@
 
 public class enemyCharacter {
 
-	public enemyCharacter() {
-		// TODO Auto-generated constructor stub
+	int hp;
+	int max_hp;
+	
+	int atk;
+	int base_atk;
+	
+	int def;
+	int base_def;
+	
+	int coins;
+	int max_coins;
+	
+	public enemyCharacter(int max_hp, int base_atk, int base_def, int max_coins) 
+	{
+		this.max_hp = max_hp;
+		this.base_atk = base_atk;
+		this.base_def = base_def;
+		this.max_coins = max_coins;
+		
+		hp = max_hp;
+		atk = base_atk;
+		def = base_def;
+		coins = max_coins;
 	}
+
+	
 
 }
--- src/rpgMain.java
+++ src/rpgMain.java
@@ -4,6 +4,7 @@ public class rpgMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		playerCharacter player = new playerCharacter(100, 10, 10, 1000);
+		enemyCharacter enemy = new enemyCharacter();
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("RPGame: The RPG made for you!");
